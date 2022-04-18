7fca64a90ab23f041101d815c86ad755400a9569
Oleksandr Orlov <job.aleksandrorlov@gmail.com>
2022-04-18 13:02:48 +0800

    Added background, actions, text colors to Appearance.

--- MoneyTracker/MoneyTrackerPresentation/Appearance/Appearance.swift
+++ MoneyTracker/MoneyTrackerPresentation/Appearance/Appearance.swift
@@ -15,8 +15,22 @@ protocol Appearance {
     
     var primaryBackground: UIColor { get }
     var secondaryBackground: UIColor { get }
+    var tertiaryBackground: UIColor { get }
+    var selectedBackground: UIColor { get }
+    var primaryActionBackground: UIColor { get }
+    var primaryActionText: UIColor { get }
+    var primaryActionBackgroundDisabled: UIColor { get }
+    var primaryActionTextDisabled: UIColor { get }
+    var successActionBackground: UIColor { get }
+    var successActionText: UIColor { get }
+    var dangerousActionBackground: UIColor { get }
+    var dangerousActionText: UIColor { get }
+    var accent: UIColor { get }
     var primaryText: UIColor { get }
     var secondaryText: UIColor { get }
-    var accent: UIColor { get }
+    var tertiaryText: UIColor { get }
+    var successText: UIColor { get }
+    var dangerousText: UIColor { get }
+    var transparent: UIColor { get }
     
 }
