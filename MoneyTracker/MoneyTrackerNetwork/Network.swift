8c12d7a4d97696ac0963ee363cda240bce0bbc23
job.ihormyroniuk@gmail.com <Ihor Myroniuk>
2022-06-25 15:19:04 +0300

    Refactor

--- MoneyTracker/MoneyTrackerNetwork/Network.swift
+++ MoneyTracker/MoneyTrackerNetwork/Network.swift
@@ -8,22 +8,22 @@
 import Foundation
 import AFoundation
 import Fawazahmed0CurrencyApi
-typealias Fawazahmed0CurrencyApiVersion1 = Fawazahmed0CurrencyApi.ApiVersion1
-public typealias Fawazahmed0CurrencyApiVersion1Currency = Fawazahmed0CurrencyApi.ApiVersion1Currency
 
 public class Network {
     
-    private let urlSession = URLSession.shared
+    private let urlSession: URLSession
+    
+    // MARK: - Initialization
     
     public init() {
-        
+        urlSession = URLSession.shared
     }
     
     // MARK: - Fawazahmed0CurrencyApi
     
-    private let fawazahmed0CurrencyApiVersion1Jsdelivr = Fawazahmed0CurrencyApiVersion1.jsdelivr
+    private let fawazahmed0CurrencyApiVersion1Jsdelivr = Fawazahmed0CurrencyApi.ApiVersion1.jsdelivr
     
-    public func latestCurrenciesCurrency(_ currency: Fawazahmed0CurrencyApiVersion1Currency, completionHandler: @escaping (Result<URLSession.HttpExchangeDataTaskResponse<ApiVersion1LatestCurrenciesParsedResponse>, Swift.Error>) -> Void) {
+    public func latestCurrenciesCurrency(_ currency: Fawazahmed0CurrencyApi.ApiVersion1Currency, completionHandler: @escaping (Result<URLSession.HttpExchangeDataTaskResponse<ApiVersion1LatestCurrenciesParsedResponse>, Swift.Error>) -> Void) {
         do {
             let requestData = Fawazahmed0CurrencyApi.ApiVersion1LatestCurrenciesRequestData(currency: currency)
             let httpExchange = fawazahmed0CurrencyApiVersion1Jsdelivr.latestCurrencies(requestData: requestData)
